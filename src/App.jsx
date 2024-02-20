b72f1207ee1c56af5ff6717c70ef0a3dc04f1102
unknown <shantanupawar101@gmail.com>
2024-02-20 10:21:08 +0530

    added responsivness

--- src/App.jsx
+++ src/App.jsx
@@ -1,74 +1,68 @@
-import React from 'react'
-import "./styles/global.scss"
-import Home from './pages/home/Home'
-import Products from './pages/products/Products';
-import Users from './pages/users/Users';
-import Product from './pages/product/product';
-import User from "./pages/user/User"
-import Login from './pages/login/Login';
-import Navbar from "./components/navbar/Navbar"
-import Menu from "./components/menu/Menu"
-import Footer from "./components/footer/Footer"
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Outlet
-} from "react-router-dom";
-
+import React from "react";
+import "./styles/global.scss";
+import Home from "./pages/home/Home";
+import Products from "./pages/products/Products";
+import Users from "./pages/users/Users";
+import Product from "./pages/product/Product";
+import User from "./pages/user/User";
+import Login from "./pages/login/Login";
+import Navbar from "./components/navbar/Navbar";
+import Menu from "./components/menu/Menu";
+import Footer from "./components/footer/Footer";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
 const App = () => {
-
   const Layout = () => {
-    return(
-      <div className='main'>
+    return (
+      <div className="main">
         <Navbar />
-        <div className='container'>
-          <div className='menucontainer'>
+        <div className="container">
+          <div className="menucontainer">
             <Menu />
           </div>
-          <div className='contentcontainer'>
+          <div className="contentcontainer">
             <Outlet />
           </div>
         </div>
         <Footer />
       </div>
-    )
-  }
+    );
+  };
 
   const router = createBrowserRouter([
     {
       path: "/",
       element: <Layout />,
-      children:[
+      children: [
         {
           path: "/",
-          element: <Home />
+          element: <Home />,
         },
         {
           path: "/users",
-          element: <Users />
+          element: <Users />,
         },
         {
           path: "/products",
-          element: <Products />
+          element: <Products />,
         },
         {
           path: "/users/:id",
-          element: <User />
+          element: <User />,
         },
         {
           path: "/products/:id",
-          element: <Product />
-        }
-      ]
+          element: <Product />,
+        },
+      ],
     },
     {
       path: "/login",
-      element: <Login />
-    }
+      element: <Login />,
+    },
   ]);
 
-  return <RouterProvider router={router}/>
-}
+  return <RouterProvider router={router} />;
+};
 
-export default App
+export default App;
